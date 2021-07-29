3836e00c9e625934bbe847f417387216f7dfdbc2
PhumipatWave <83903422+PhumipatWave@users.noreply.github.com>
2021-07-29 16:51:42 +0700

    Add project files.

---
+++ Hashtable_HW/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hashtable_HW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Hashtable hashtable;
+        public MainWindow()
+        {
+            InitializeComponent();
+            hashtable = new Hashtable();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            hashtable.Add(Key.Text, Value.Text);
+        }
+
+        private void Show_Click(object sender, RoutedEventArgs e)
+        {
+            ICollection icollection = hashtable.Keys;
+            foreach (string key in icollection)
+            {
+                MessageBox.Show(hashtable[key].ToString());
+            }
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+            hashtable.Remove(Key.Text);
+        }
+    }
+}
